4ab6213e68da3e0a09242c4000ade03b4982da27
offfahad <mughalfahad544@gmail.com>
2023-07-05 12:58:08 +0500

    calculation-added

--- app/src/main/java/com/example/artspaceapp/MainActivity.kt
+++ app/src/main/java/com/example/artspaceapp/MainActivity.kt
@@ -5,20 +5,25 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardElevation
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
@@ -26,8 +31,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -94,14 +97,50 @@ fun ArtWithTitle(ArtPicture: Int, ArtTitle: String, ArtistName: String, modifier
     }
 }
 
+@Composable
+fun ArtSpaceMain(){
+    var result by remember {
+        mutableStateOf(0)
+    }
+    when (result) {
+        1-> ArtWithTitle(
+            ArtPicture = R.drawable.edvardmunch,
+            ArtTitle = stringResource(R.string.TEdvardMunch),
+            ArtistName = stringResource(id = R.string.EdvardMunch)
+        )
+        2-> ArtWithTitle(
+            ArtPicture = R.drawable.johannes,
+            ArtTitle = stringResource(R.string.TJohannes),
+            ArtistName = stringResource(id = R.string.JohannesVermeer)
+        )
+        3-> ArtWithTitle(
+            ArtPicture = R.drawable.georges,
+            ArtTitle = stringResource(R.string.TGeorgesSeurat),
+            ArtistName = stringResource(id = R.string.George)
+        )
+        4-> ArtWithTitle(
+            ArtPicture = R.drawable.selfportrait,
+            ArtTitle = stringResource(R.string.TSelfPortrait),
+            ArtistName = stringResource(id = R.string.SelfPortrait)
+        )
+        5-> ArtWithTitle(
+            ArtPicture = R.drawable.eugenedelacroix,
+            ArtTitle = stringResource(R.string.TEugeneDelacroix),
+            ArtistName = stringResource(id = R.string.EugeneDelacroix)
+        )
+        else -> ArtWithTitle(
+            ArtPicture = R.drawable.jean_antoine,
+            ArtTitle = stringResource(R.string.TJeanAntoine),
+            ArtistName = stringResource(id = R.string.JeanAntoine)
+        )
+    }
+
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ArtSpaceAppTheme {
-        ArtWithTitle(
-            ArtPicture = R.drawable.edvardmunch,
-            ArtTitle = stringResource(R.string.TGeorgesSeurat),
-            ArtistName = stringResource(R.string.EdvardMunch)
-        )
+        ArtSpaceMain()
     }
 }
