08f5daa79439a1c6f0395558b691c799e0eda3e7
offfahad <mughalfahad544@gmail.com>
2023-07-05 12:04:27 +0500

    art-space-app

---
+++ app/src/main/java/com/example/artspaceapp/MainActivity.kt
@@ -0,0 +1,107 @@
+package com.example.artspaceapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardElevation
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.artspaceapp.ui.theme.ArtSpaceAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ArtSpaceAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ArtWithTitle(ArtPicture: Int, ArtTitle: String, ArtistName: String, modifier: Modifier = Modifier) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Image(
+            painter = painterResource(id = ArtPicture),
+            contentDescription = null,
+            modifier = Modifier
+                .padding(20.dp)
+                .height(300.dp)
+                .width(300.dp)
+                .border(border = BorderStroke(2.dp, Color.Gray), shape = RectangleShape)
+                .shadow(elevation = 4.dp)
+                .padding(10.dp)
+        )
+        Card(
+            modifier = Modifier
+                .padding(horizontal = 45.dp, vertical = 100.dp)
+
+        ){
+            Column(modifier = Modifier.padding(16.dp)) {
+                Text(
+                    text = ArtTitle,
+                    fontSize = 25.sp,
+                    textAlign = TextAlign.Start,
+                    lineHeight = 30.sp
+                )
+                Text(
+                    text = ArtistName,
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = modifier.padding(top = 8.dp)
+                )
+            }
+        }
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ArtSpaceAppTheme {
+        ArtWithTitle(
+            ArtPicture = R.drawable.edvardmunch,
+            ArtTitle = stringResource(R.string.TGeorgesSeurat),
+            ArtistName = stringResource(R.string.EdvardMunch)
+        )
+    }
+}
